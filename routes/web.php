5f7d2c331a1f30737d2c1fcb2391a98133025b37
starrvyaa <safinadyah19@gmail.com>
2025-03-17 10:12:44 +0700

    try

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
+// l
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/contact', [ContactController::class, 'index'])->name('contact');
